2433cc4eaf59b1402f1478f840a719f4b42cc353
Fedemartinez01 <fedemartinezl2001@gmail.com>
2022-06-20 02:15:13 -0300

    TP 4

---
+++ Martinez.Federico.2A.TP4/EntidadesTP3/Excepciones/ArchivoNoEncontradoExcepcion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntidadesTP4.Excepciones
+{
+    public class ArchivoNoEncontradoExcepcion : Exception
+    {
+        public ArchivoNoEncontradoExcepcion() : this(null)
+        {
+
+        }
+        public ArchivoNoEncontradoExcepcion(Exception exception) : base("\nNo se ha encontrado el archivo", exception)
+        {
+
+        }
+    }
+}
